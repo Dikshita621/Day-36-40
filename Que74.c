7b15a9afc2f4a6960b8fba9b1deea5dd70808e8f
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-10-02 22:42:16 +0530

    Create Que74.c

---
+++ Que74.c
@@ -0,0 +1,48 @@
+/*Q74: Find the transpose of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+1 4
+2 5
+3 6
+
+*/
+#include <stdio.h>
+
+int main() {
+    int rows, columns;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &columns);
+
+    int matrix[rows][columns];
+    int transpose[columns][rows];
+
+    printf("Enter the matrix elements:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Compute transpose
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for (int i = 0; i < columns; i++) {
+        for (int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
